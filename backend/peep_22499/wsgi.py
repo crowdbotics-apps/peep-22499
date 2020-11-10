326672e462fa5867ff0c2cbf4ca98492e31d7ac7
Crowdbotics <team@crowdbotics.com>
2020-11-10 23:46:43 +0000

    Update models

--- backend/peep_22499/wsgi.py
+++ backend/peep_22499/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'peep_22499.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "peep_22499.settings")
 
 application = get_wsgi_application()
